39297b98662534f03690e5101d648e5421a2ea65
Alejandro Roa <alejandro.globalesm.com>
2021-11-23 16:43:09 -0400

    some posts

--- components/mainBlog.js
+++ components/mainBlog.js
@@ -7,7 +7,7 @@ export default function MainBlog() {
       <section className="blog" id="main">
         <div className="blog__container">
           <div className="card-blog">
-            <a href="#">
+            <a href="/blog/es/implementar_una_buena_ui">
               <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
                 width="800" height="600" />
               <div className="labels-container">
@@ -21,99 +21,13 @@ export default function MainBlog() {
             </a>
           </div>
           <div className="card-blog">
-            <a href="#">
+          <a href="/blog/es/state-management-en-angular-ngxs">
               <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
                 width="800" height="600" />
               <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
+                <label className="labelPosts">State Management en Angular: Ngxs</label>
                 <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="card-blog">
-            <a href="#">
-              <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
-                width="800" height="600" />
-              <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
-                <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="card-blog">
-            <a href="#">
-              <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
-                width="800" height="600" />
-              <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
-                <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="card-blog">
-            <a href="#">
-              <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
-                width="800" height="600" />
-              <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
-                <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="card-blog">
-            <a href="#">
-              <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
-                width="800" height="600" />
-              <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
-                <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="card-blog">
-            <a href="#">
-              <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
-                width="800" height="600" />
-              <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
-                <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="card-blog">
-            <a href="#">
-              <Image src="/img/pc2.jpg" alt="know" className="knowledge_img"
-                width="800" height="600" />
-              <div className="labels-container">
-                <label className="labelPosts">Desarrollar una buena UI</label>
-                <p className="labelPostsResume">
-                  Debemos tener en cuenta cuatro cosas elementales,
-                  la arquitectura del website, las herramientas a usar, la calidad de los assets y
-                  la tecnología a implementar.
+                  Un manejador de estados similar al flow de redux para Angular
                 </p>
               </div>
             </a>
