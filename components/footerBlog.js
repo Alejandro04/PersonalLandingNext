e8674920360a35762db6e1389972180303d3a828
Alejandro Roa <alejandro.globalesm.com>
2021-11-25 10:48:24 -0400

    footer

--- components/footerBlog.js
+++ components/footerBlog.js
@@ -2,20 +2,6 @@ export default function FooterBlog() {
   return (
     <>
       <footer className="footer">
-        <div className="footer__container">
-          <div>
-            <span>+58 04247395321</span>
-          </div>
-          <div>
-            <span> <a href="https://github.com/Alejandro04" target="_blank">Github</a> </span>
-          </div>
-          <div>
-            <span> <a href="https://www.linkedin.com/in/alejandroroa/" target="_blank">Linkedin</a> </span>
-          </div>
-          <div>
-            <span> <a href="https://alejandroroa.medium.com/" target="_blank">Medium</a> </span>
-          </div>
-        </div>
         <div className="footer__down">
           © Copyright by 2021 Alejandro Roa
         </div>
