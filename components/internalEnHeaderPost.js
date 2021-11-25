e726d4e131f5beb909d71c78917ac5eeaf3c47fe
Alejandro Roa <alejandro.globalesm.com>
2021-11-25 14:30:44 -0400

    fix

--- components/internalEnHeaderPost.js
+++ components/internalEnHeaderPost.js
@@ -4,13 +4,15 @@ import { useState, useEffect } from 'react';
 
 export default function internalHeaderBlog() {
   const [title, setTitle] = useState();
-
   const router = useRouter()
-  const dataArray = router.asPath.split('/')
-  let lowerTitle = ""
+
+  if(router){
+    const dataArray = router.asPath.split('/')
+    let lowerTitle = ""
   
-  if(dataArray.length > 0 ){
-    lowerTitle = dataArray[3].replaceAll('-', ' ')
+    if(dataArray.length > 0 ){
+      lowerTitle = dataArray[3].replaceAll('-', ' ')
+    }
   }
 
   useEffect(() => {
