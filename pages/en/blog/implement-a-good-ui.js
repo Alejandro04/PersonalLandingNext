ed92d190e601ff498cb6cbbd4705f9356e7eb12b
Alejandro Roa <alejandro.globalesm.com>
2021-11-26 11:14:02 -0400

    EN post

--- pages/en/blog/implement-a-good-ui.js
+++ pages/en/blog/implement-a-good-ui.js
@@ -9,165 +9,167 @@ export default function Post() {
       <section className="post" id="main">
         <div className="post__container container">
           <section class="items-detail-container" id="post">
-            <article>Para nosotros los desarrolladores que venimos de hacer backend suele ser un poco
-              confuso el cómo desarrollar buenas interfaces de usuario, en mi caso personal, me costaba
-              entender qué pasos seguir para lograr una meta en específico y ello va desde la maquetación,
-              los estilos, el responsive design, la implementación de los assets, las fuentes, la paleta de colores,
-              y qué tipo de formato usar para imágenes o íconos. Además el SEO: cómo configurarlo de forma correcta.
+            <article>For us developers coming from backend it is usually a bit confusing how to develop good user interfaces.
+              confusing how to develop good user interfaces, in my personal case, I found it difficult to understand what steps to follow to achieve a
+              understand what steps to follow to achieve a specific goal and this goes from the layout,
+              the styles, the responsive design, the implementation of the assets, the fonts, the color palette,
+              and what kind of format to use for images or icons. Plus SEO: how to set it up correctly.
             </article>
             <article>
-              El 100% de los proyectos en los cuales participé y tuve la tarea de desarrollar la UI seleccioné Bootstrap
-              como framework porque en mi desconocimiento e incluso algo de miedo, buscaba un atajo para hacer algo
-              decente.
-              Y si, como pueden suponer, los resultados pocas veces fueron buenos.
+              100% of the projects in which I participated and had the task of developing the UI I selected Bootstrap
+              as framework because in my ignorance and even some fear, I was looking for a shortcut to do something decent.
+              decent.
+              And yes, as you can guess, the results were rarely good.
             </article>
             <article>
-              Todo esto también se debe a que pocas veces he trabajado en oficina, con personas buenas haciendo UI
-              entonces es difícil entender cómo comenzar, qué tener en cuenta o cómo ayudarse. Esto sumado al poco
-              interés que tenía
-              por esta materia, ya que estando en esos momentos trabajando solo en el backend me tenia conforme.
+              All this is also because I have rarely worked in an office, with good UI people, so it is difficult to understand how to start, what to consider or how to help yourself.
+              so it is difficult to understand how to start, what to take into account or how to help yourself. This added to the little
+              interest I had
+              I had little interest in this subject, since at that time I was working only in the backend and I was satisfied with it.
             </article>
             <article>
-              Luego de esa larga introducción ahora si, entremos en materia:
-            </article>
-            <article>
-              <span class="post-subtitle">
-                Arquitectura el website:
-              </span>
-              Esto aplica para todo tipo de proyecto, en este caso,
-              al desarrollar una buena UI necesitas entender cómo se va comportar el proyecto para que puedas distribuir
-              de una forma
-              adecuada los items que vas a crear. Este blog está desarrollado con puro Html, Css y Javascript, no he usado
-              ningún framework para ello,
-              mi idea es migrar a Gatsby luego pero eso es otro tema, el punto es que esta aplicación es una web estática
-              y la
-              finalidad de la misma es desplegar el proyecto rápido, ahorrando tiempo, por
-              ende la arquitectura que estoy usando puede
-              no ser muy escalable pero por los momentos cubre lo que necesito. Además, al montar un website que va
-              implementar SEO se debe tener en cuenta que,
-              las SPA no funcionan para ello. Por esto es que es IMPORTANTE saber la finalidad del website, si se va
-              implementar SEO o no
-              y si los requerimientos del equipo de diseño son los adecuados para esto (por algo se los digo)
+              After this long introduction, let's get down to business:
             </article>
             <article>
               <span class="post-subtitle">
-                Herramientas a usar:
+                Website architecture:
               </span>
-              Si tienes conocimiento de diseño gráfico o diseño de interfaces tal vez se te haga fácil poder configurar
-              una paleta de colores para
-              cada sección y los assets adecuados pero, ¿qué sucede con los developers que no tenemos este conocimiento?
-              entendí que es importante apoyarse
-              en herramientas que nos ayudan en varios puntos:
+              This applies to all types of projects, in this case,
+              developing a good UI you need to understand how the project is going to behave so that you can distribute it in a
+              in an adequate
+              the items you are going to create. This blog is developed with pure Html, Css and Javascript, I haven't used any framework for it.
+              any framework for it,
+              my idea is to migrate to Gatsby later but that's another topic, the point is that this application is a static website
+              and the
+              the purpose of it is to deploy the project quickly, saving time, therefore the
+              the architecture I'm using may not be very
+              may not be very scalable but for the moment it covers what I need. Also, when setting up a website that is going to
+              implement SEO it should be taken into account that,
+              SPA's don't work for it. This is why it is IMPORTANT to know the purpose of the website, if you are going to
+              implement SEO or not
+              and if the requirements of the design team are adequate for this (I am telling you for a reason).
+            </article>
+            <article>
+              <span class="post-subtitle">
+                Tools to use:
+              </span>
+              If you have a background in graphic design or interface design, you may find it easy to configure a color palette for
+              a color palette for each section
+              each section and the appropriate assets but, what happens with developers who don't have this knowledge?
+              I understood that it is important to rely
+              in tools that help us in several points:
             </article>
             <article>
               <span class="post-subtitle-subtitle">
                 <a href="https://uigradients.com/" target="_blank">UIGRADIENTS: </a>
               </span>
-              Tienes una buena alternativas de gradientes para usar en tu sitio web.
+              You have a good choice of gradients to use on your website.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
                 <a href="https://webgradients.com/" target="_blank">WEBGRADIENTS: </a>
               </span>
-              Otra buena herramienta para que puedas seleccionar los gradientes para tu website.
+              Another good tool for you to select gradients for your website.
+              You have a good choice of gradients to use on your website.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
                 <a href="https://flatuicolors.com/" target="_blank">FLATUICOLORS: </a>
               </span>
-              Permite seleccionar paleta de colores.
+              Allows you to select the color palette.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
                 <a href="https://bennettfeely.com/clippy/" target="_blank">BENNETTFEELY: </a>
               </span>
-              Permite simular en base a coordenadas la funcion clip-path de css para aplicarlo a nuestro sitios.
+              It allows to simulate based on coordinates the function clip-path of css to apply it to our sites.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
                 <a href="https://fontawesome.com/" target="_blank">FONTAWESOME: </a>
               </span>
-              Este lugar es famoso, puedes bajar los mejores iconos para tu website.
+              This place is famous, you can download the best icons for your website.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
                 <a href="https://fonts.google.com/" target="_blank">GOOGLE FONTS: </a>
               </span>
-              Algo que no puede faltar, seleccionar fuentes adecuadas para nuestro sitio.
+              Something that can not be missed, select fonts suitable for our site.
             </article>
             <article>
               <span class="post-subtitle">
-                La calidad de los assets:
+                The quality of the assets:
               </span>
-              ¿Te han dado assets en un documento de word? ¿te han dado imágenes gigantes y muy pequeñas para colocar
-              en una sola sección que tiene relación?
-              De los assets depende en gran pante el rendimiento de nuestro website al cargar por primera vez, debes tener
-              en cuenta lo siguiente:
+              Have you been given assets in a word document? have you been given giant, very small images to place in a single section that is related?
+              in a single section that is related?
+              The performance of our website when loading for the first time depends largely on the assets, you must take into account the following
+              the following into account:
             </article>
             <article>
               <span class="post-subtitle-subtitle">
-                Intenta en lo posible que las imágenes, logos e iconos sean SVG:
+                Try as much as possible to use SVG images, logos and icons:
               </span>
-              es un tipo de imágen digital formada mediante gráficos vectoriales, es decir, las imágenes que se guardan en
-              este formato deben poder ser
-              representada como gráficos vectoriales y funciones matemáticas,
-              a diferencia de lo que ocurre con las imágenes de mapa de bits (JPG, PNG, etc)
-              donde cada pixel de la imágen tiene su propia información.
+              is a type of digital image made up of vector graphics, i.e., images saved in this format must be able to be
+              this format must be able to be
+              represented as vector graphics and mathematical functions,
+              unlike what happens with bitmap images (JPG, PNG, etc.), where each pixel of the image has
+              where each pixel of the image has its own information.
             </article>
             <article>
-              Cuando tenemos una imágen de mapa de bits y la ampliamos podemos apreciar una pérdida notable de calidad,
-              llegando incluso a ser totalmente borrosa si la ampliamos demasiado. Esto se debe a que al hacer zoom sobre
-              una imagen
-              con mapa de bits estamos ampliando cada pixel de forma individual, lo que genera una pérdida de calidad.
+              When we have a bitmap image and we enlarge it we can appreciate a noticeable loss of quality,
+              even becoming totally blurred if we enlarge it too much. This is due to the fact that when zooming in on an image
+              a bitmap image
+              bitmap image we are enlarging each pixel individually, which generates a loss of quality.
             </article>
             <article>
-              Por el contrario, al hacer zoom a una imágen vectorial, gracias a las propiedades matemáticas de los
-              vectores, la imagen vuelve a dibujarse de nuevo multiplicando su vector por el valor del zoom que estemos
-              realizando.
-              De esta manera, la imágen se redibuja desde cero con una calidad del 100% y sin ningún tipo de pérdida.
+              On the other hand, when zooming in on a vector image, thanks to the mathematical properties of the
+              the image is redrawn by multiplying its vector by the value of the zoom we are performing.
+              we are performing.
+              In this way, the image is redrawn from scratch with 100% quality and without any loss.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
-                El tamaño de las imágenes deben ser las adecuadas para cada sección:
+                The size of the images should be appropriate for each section:
               </span>
-              Si tienes que usar Css para redimensionar una imágen en tu website estás cometiendo un error (yo debo
-              arreglar ese detallito en un punto en este website)
-              NO es una buena práctica, daña el rendimiento del sitio.
+              If you have to use Css to resize an image on your website you are making a mistake (I have to fix that detail at one point in this website).
+              fix that little detail at a later point in this website)
+              It is NOT a good practice, it damages the performance of the site.
             </article>
             <article>
               <span class="post-subtitle-subtitle">
-                Si no es posible que una imágen sea formato SVG entonces optimiza dicha imágen para la web:
+                If it is not possible for an image to be in SVG format then optimize the image for the web:
               </span>
-              Existen algunas herramientas online que permite esto, solo debes subir dicho asset y descargarlo optimizado.
+              There are some online tools that allow this, you just have to upload the asset and download it optimized.
+              If it is not possible for an image to be in SVG format then optimize the image for the web:
             </article>
             <article>
               <span class="post-subtitle">
-                Las tecnologías a implementar:
+                Technologies to be implemented:
               </span>
-              Aprende Flexbox y CssGrid. no intentes usar un framework porque pareciera ser un "atajo" para maquetar por
-              la distribución de los grids, la mayoría de veces esas configuraciones no cumplen las expectativas de los
-              proyectos
-              que tenemos que desarrollar, no la cagues como yo xD.
+              Learn Flexbox and CssGrid. don't try to use a framework because it seems to be a "shortcut" for layout by
+              the layout of the grids, most of the time those configurations do not meet the expectations of the projects.
+              projects
+              we have to develop, don't mess up like me xD.
             </article>
             <article>
-              Bueno amigos, espero que les sirva este post, estoy feliz porque al fin creo que soy bueno haciendo UI y eso
-              me abre la mente
-              para entender otras cosas importantes que tal vez no tiene que ver mucho con código ¿de qué sirve montar un
-              website si no hace dinero?
-              se las dejo por ahí. Saludos!
+              Well friends, I hope you find this post useful, I'm happy because at last I think I'm good at making UI and it
+              opens my mind
+              to understand other important things that may not have much to do with code, what's the point of building a website if it doesn't make money?
+              website if it doesn't make money?
+              I'll leave it there. Greetings!
             </article>
           </section>
 
           <section class="hashtags-container">
-            <div class="hashtag">#Desarrollo UI</div>
-            <div class="hashtag">#Herramientas UI</div>
+            <div class="hashtag">#UI Develop</div>
+            <div class="hashtag">#UI Tools</div>
             <div class="hashtag">#Flexbox</div>
             <div class="hashtag">#CssGrid</div>
             <div class="hashtag">#Javascript</div>
           </section>
         </div>
       </section>
-      
+
       <FooterBlog />
       <Script
         id="nav-js"
